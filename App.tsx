eecde68c0d5959f9e92fa051abb49dac758058b4
Mevlutyildirim <Mevlutyildirim@users.noreply.github.com>
2020-07-14 10:52:40 +0300

    dockerfile has been added

--- App.tsx
+++ App.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {GiftedChat} from 'react-native-gifted-chat';
 
 export default function App() {
+  
+  const [messages, useMessages] = useState([{
+    _id: 1,
+    text: 'Hello mevlüt ',
+    createdAt: new Date(),
+    user: {
+      _id: 2,
+      name: 'React Native',
+      avatar: 'https://placeimg.com/140/140/any',
+    }}]);
+
+    onSendMessages(messages = []) {
+      useMessages(previousState => ({
+        messages: GiftedChat.append(previousState.messages, messages),
+      }))
+    }
+  
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+      <GiftedChat messages={messages} onSend={messages => onSendMessages(messages)}  user={{
+          _id: 1,
+        }}
+      />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
